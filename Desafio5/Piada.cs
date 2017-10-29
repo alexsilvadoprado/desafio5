b2665df361568fec51b0dead875e93696983f422
Tamirys <tamygemeas@gmail.com>
2017-10-29 03:03:55 -0300

    Primeiro commit

---
+++ Desafio5/Piada.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio5
+{
+    public class Piada
+    {
+        public string Nome { get; set; }
+        public string Url { get; set; }
+        public string Corpo { get; set; }
+        public string Categoria { get; set; }
+        public DateTime DataPublicacao { get; set; }
+        public int VotosPositivos { get; set; }
+        public int VotosNegativos { get; set; }
+        public int VotosMedios { get; set; }
+    }
+}
---
+++ Desafio5/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Desafio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://www.osvigaristas.com.br/piadas/";
+
+            WebClient webClient = new WebClient();
+
+            string resposta = webClient.DownloadString(url);
+
+            int indiceInicial = resposta.IndexOf("<article");
+            int indiceFinal = resposta.LastIndexOf("</article>");
+
+            string piadas = resposta.Substring(indiceInicial, indiceFinal - indiceInicial + 10);
+            piadas = Regex.Replace(piadas, ">(\\s)*<", "><");
+            DeserializarPiadas(piadas);
+        }
+
+        private static void DeserializarPiadas(string piadas)
+        {
+            string[] split = Regex.Split(piadas, "article>");
+            foreach (var s in split)
+            {
+                Piada piada = new Piada();
+                int indiceInicioUrl = s.IndexOf("<h4><a href=");
+                int indiceFimUrl = s.IndexOf(".html");
+                piada.Url = s.Substring(indiceInicioUrl + 13, indiceFimUrl - indiceInicioUrl - 8);
+
+                int indiceFimNome = s.IndexOf("</a></h4>");
+                piada.Nome = s.Substring(indiceFimUrl + 7, indiceFimNome - indiceFimUrl - 7);
+
+                int indiceInicioCorpo = s.IndexOf("<p>");
+                int indiceFimCorpo = s.LastIndexOf("</p>");
+                string corpo = s.Substring(indiceInicioCorpo, indiceFimCorpo - indiceInicioCorpo + 4);
+                string[] splitCorpo = Regex.Split(corpo, "><");
+                corpo = "";
+                foreach (var p in splitCorpo)
+                {
+                    int indiceInicial = p.IndexOf("p>");
+                    int indiceFinal = p.IndexOf("</");
+                    corpo += p.Substring(indiceInicial + 2, indiceFinal - indiceInicial - 2) + "\n";
+                }
+                piada.Corpo = corpo;
+
+                int indiceInicioCategoria = s.IndexOf("<li>");
+                int indiceFimCategoria = s.LastIndexOf("</li>");
+                string categorias = s.Substring(indiceInicioCategoria, indiceFimCategoria - indiceInicioCategoria + 5);
+                string[] splitCategorias = Regex.Split(categorias, "</a></li>");
+                categorias = "";
+                foreach (var cat in splitCategorias)
+                {
+                    int indiceNomeCategoria = cat.LastIndexOf(">");
+                    categorias += cat.Substring(indiceNomeCategoria + 1, cat.Length - indiceNomeCategoria - 1) + "\n";
+                }
+                piada.Categoria = categorias;
+            }
+        }
+    }
+}
